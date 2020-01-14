62b702d723f6c753b2145853db020d443ffba848
monochromegane <dev.kuro.obi@gmail.com>
2020-01-14 11:37:04 +0900

    Initial commit.

---
+++ lib/random_multivariate_normal.rb
@@ -0,0 +1,6 @@
+require "random_multivariate_normal/version"
+
+module RandomMultivariateNormal
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/random_multivariate_normal/version.rb
@@ -0,0 +1,3 @@
+module RandomMultivariateNormal
+  VERSION = "0.1.0"
+end
